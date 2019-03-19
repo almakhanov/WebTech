9fcf03e854a074606e68da956684824af11f71e4
qalqaman22dev <qalqaman22dev@gmail.com>
2019-03-19 21:02:52 +0600

    Added cart component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import {ProductComponent} from './product/product.component';
 import {MenuComponent} from './components/menu/menu.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {HeaderComponent} from './components/header/header.component';
+import {CartComponent} from './cart/cart.component';
 
 const routes: Routes = [
-  { path: 'products', component: ProductComponent }
+  { path: 'products', component: ProductComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
