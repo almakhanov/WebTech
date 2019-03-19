c96d22e78cbec33038227733eb16849eaf258d27
limbowtnb <limbovtnb@gmail.com>
2019-03-19 22:37:14 +0600

    slider sdelano

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProductComponent} from './product/product.component';
+import {SliderComponent} from './slider/slider.component';
 import {MenuComponent} from './components/menu/menu.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {HeaderComponent} from './components/header/header.component';
 
 const routes: Routes = [
-  { path: 'products', component: ProductComponent }
-];
+  { path: 'products', component: ProductComponent},
+  { path: '', component: SliderComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
